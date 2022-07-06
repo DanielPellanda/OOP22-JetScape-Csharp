349265d8cdfbe1775fc90208e5994124de205d15
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2022-07-06 18:04:50 +0200

    implemented collisions handler and checker

---
+++ JetScape/GiacomoAmadio/Collisions/CollisionChecker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using JetScape.game.utility;
+using JetScape.game.logics.entities;
+using JetScape.game.logics.entities.player;
+using JetScape.Collisions.Hitbox;
+
+namespace JetScape.Collisions
+{
+    public class CollisionsChecker
+    {
+        private Dictionary<EntityType, ISet<Entity>> _entities;
+
+        private Player _player;
+
+        private Queue<Entity> _collisions;
+
+        public CollisionsChecker(Dictionary<EntityType, ISet<Entity>> entities, Player p)
+        {
+            _entities = entities;
+            _collisions = new Queue<Entity>();
+            _player = p;
+        }
+
+        public Entity NextToHandle() 
+        {
+            Entity entity = null;
+            _collisions.TryDequeue(out entity);
+            return entity;
+        }
+
+        public void updateCollisions()
+        {
+            foreach ( KeyValuePair<EntityType, ISet<Entity>> entry in _entities )
+            {
+                if (entry.Key != EntityType.PLAYER)
+                {
+                    foreach (Entity entity in entry.Value)
+                    {
+                        if (collides(entity.))
+                        {
+                            _collisions.Enqueue(entity);
+                        }
+                    }
+                }
+            }
+        }
+        private bool collides(IHitbox entity)
+        {
+            foreach (Rectangle player in _player.GetRectangles())
+            {
+                foreach (Rectangle target in entity.GetRectangles())
+                {
+                    if (player.IntersectsWith(target))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ JetScape/GiacomoAmadio/Collisions/CollisionsHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using JetScape.game.utility;
+using JetScape.game.logics.entities;
+using JetScape.game.logics.entities.player;
+
+namespace JetScape.Collisions
+{
+    public delegate void Rection(Entity e);
+    public class CollisionsHandler
+    {
+        private  CollisionsChecker cChecker; 
+
+    public CollisionsHandler(Dictionary<EntityType, ISet<Entity>> entities,  Player p)
+        {
+            this.cChecker = new CollisionsChecker(entities, p);
+        }
+
+        public void interact( Rection action )
+        {
+            cChecker.updateCollisions();
+            var entity = cChecker.NextToHandle();
+            while (entity != null)
+            {
+                action.Invoke(entity);
+                entity = cChecker.NextToHandle();
+            }
+        }
+    }
+}
