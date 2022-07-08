15849387574a3ae200c06dafd188c96a52032014
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2022-07-08 19:26:24 +0200

    implmented tests

---
+++ JetScape/GiacomoAmadio/Collisions/TestCollisions.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using JetScape.game.logics;
+using JetScape.game.logics.entities;
+using JetScape.game.logics.entities.player;
+using JetScape.game.utility;
+
+namespace JetScape.Collisions
+{
+    [TestFixture]
+    public class TestCollisions
+    {
+        private CollisionsChecker _collisionC;
+        private Logics _lh;
+        private Dictionary<EntityType, ISet<Entity>> _entities;
+        private Player _player;
+
+        [SetUp]
+        public void Setup()
+        {
+            _lh = new Logics();
+            _player = new Player(_lh);
+            _entities = new Dictionary<EntityType, ISet<Entity>>();
+            _collisionC = new CollisionsChecker(_entities, _player);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
